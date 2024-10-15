faa651c64b4101d7c3335a5a3455c912031c803d
Imad <imadhen@hotmail.fr>
2024-10-15 14:06:14 +0200

    azure function api recommandation fonctionnel

--- function_app.py
+++ function_app.py
@@ -1,5 +1,70 @@
 import azure.functions as func
 import logging
+import pandas as pd
+import pickle
+import numpy as np
+import json
+
+articles_df = pd.read_csv("articles_metadata.csv")
+#print(articles_df.head())
+
+#print('hi')
+
+
+# Save to file in the current working directory
+
+pkl_filename = "pickle_surprise_model_KNNWithMeans.pkl"
+
+# Load from file
+with open(pkl_filename, 'rb') as file:
+    pickle_model = pickle.load(file)
+
+#print(pickle_model)
+
+def predict_best_category_for_user(user_id):
+
+    articles_df = pd.read_csv("articles_metadata.csv")
+
+    # Save to file in the current working directory
+    pkl_filename = "pickle_surprise_model_KNNWithMeans.pkl"
+
+    # Load from file
+    with open(pkl_filename, 'rb') as file:
+        pickle_model = pickle.load(file)
+
+
+    predictions = {}
+    
+    #Category 1 to 460
+    for i in range(1, 460):
+        _, cat_id, _, est, err = pickle_model.predict(user_id, i)
+        
+        #Keep prediction only if we could keep it.
+        if (err != True):
+            predictions[cat_id] = est
+    
+    best_cats_to_recommend = dict(sorted(predictions.items(), key=lambda x: x[1], reverse=True)[:5])
+    
+    # Obtenir les articles recommandés pour chaque catégorie
+    recommended_articles = []
+    for key, _ in best_cats_to_recommend.items():
+        article_id = int(articles_df[articles_df['category_id'] == key]['article_id'].sample(1).values)
+        recommended_articles.append({
+            "category_id": key,
+            "article_id": article_id
+        })
+    
+    # Créer un dictionnaire pour la sortie JSON
+    output = {
+        "user_id": user_id,
+        "recommended_articles": recommended_articles,
+        "best_categories": best_cats_to_recommend
+    }
+    
+    # Retourner le dictionnaire au format JSON
+    return json.dumps(output, indent=4)
+
+
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
 
@@ -17,9 +82,13 @@ def reco_api_V4(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        recommendations = predict_best_category_for_user(name)
+        return func.HttpResponse(recommendations,
+                                 mimetype="application/json",
+                                 status_code=200)
+    
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+                "Please pass a userId on the query string or in the request body",
+                status_code=400
         )
